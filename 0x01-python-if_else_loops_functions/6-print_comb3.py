49ba2a6a895945a6f01d15eaf3f15a4580ca7516
Babalola Olaoluwa <lalaboiydvibeplug@gmail.com>
2023-03-24 04:01:07 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for number1 in range(11):
     for number2 in range(number1 + 1, 10):
-        if number1 == 8 && number2 == 9:
+        if number1 == 8 and number2 == 9:
             print("{}{}".format(number1,number2))
         else:
             print("{}{}".format(number1,number2), end=", ")
