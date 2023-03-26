7c9bb24e3adeb43a50ecf7bbdaa79fc52e1a4876
Babalola Olaoluwa <lalaboiydvibeplug@gmail.com>
2023-03-26 12:27:45 -0700

    9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    print_last_digit = (abs(number) % 10)
     print(abs(number) % 10), end="")
-    return print_last_digit
+    return (abs(number) % 10)
