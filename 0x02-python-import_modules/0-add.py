39ade13df2a736397e1976623d0c35374996d0e8
Babalola Olaoluwa <lalaboiydvibeplug@gmail.com>
2023-04-01 18:08:04 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    """print the sum of two numbers"""
     from add_0 import add
+    
     a = 1
     b = 2
-    print("{} + {} = {}\n".format(a, b, add(a,b)))
+    print("{} + {} = {}".format(a, b, add(a,b)))
