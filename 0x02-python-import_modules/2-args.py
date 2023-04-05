454b7cb53c5e5557155f2904789b42a93471dec2
Babalola Olaoluwa <lalaboiydvibeplug@gmail.com>
2023-04-04 17:50:50 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,10 @@
 if __name__ == "__main__":
     """Print the number of and list of arguments."""
     import sys
+    print_arg(sys.argv)
 
+
+def print_arg(argv):
     number = len(sys.argv) - 1
     if number == 0:
         print("{:d} 0-arguments.".format(number))
