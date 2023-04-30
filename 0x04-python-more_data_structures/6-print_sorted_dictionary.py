22906419b5cf456f87242085b463ad50506b2196
Babalola Olaoluwa <lalaboiydvibeplug@gmail.com>
2023-04-30 01:40:00 -0700

    6-print_sorted_dictionary.py

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    """Prints a dictionary by ordered keys."""
+    for keys in sorted(my_dict.keys()):
+        print('{}: {}'.format(keys, my_dict[keys]))
