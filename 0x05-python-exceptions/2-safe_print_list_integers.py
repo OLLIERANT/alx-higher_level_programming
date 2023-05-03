29734b420fcdbc45ad25130b65c7e0bd44d411b2
Babalola Olaoluwa <lalaboiydvibeplug@gmail.com>
2023-05-03 16:32:54 -0700

    2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    """Write a function that prints the first x elements of a list and only integers."""
+    indes = printed_ints = 0
+    while True:
+        try:
+            if index < x:
+                print("{:d}".format(my_list[index]), end='')
+                index += 1
+                printed_ints += 1
+            else:
+                print()
+                return printed_ints
+            except(ValueError, TypeError):
+                index += 1
