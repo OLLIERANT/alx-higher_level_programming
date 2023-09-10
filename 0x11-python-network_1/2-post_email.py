19631616c1890a1266af3dda83ddc18480a07a5a
Olaoluwa Babalola <lalaboiydvibeplug@gmail.com>
2023-09-09 17:57:26 -0700

    python-network_1

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Takes in a url and  an email
+sends a POST request to the url with the email as a parameter
+and displays the response body decoded in utf-8
+"""
+
+import sys
+import urllib.parse
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
