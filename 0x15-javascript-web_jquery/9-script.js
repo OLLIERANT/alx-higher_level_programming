f52524dfeb695dc905344519c52e58249215430a
Babalola Olaoluwa <lalaboiydvibeplug@gmail.com>
2024-02-20 22:08:25 -0800

    live update

--- 0x15-javascript-web_jquery/9-script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $.ajax({
-    type: 'GET',
-    url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
-    success: function (data) {
-      $('DIV#hello').append(data.hello);
-    }
-  });
-});
